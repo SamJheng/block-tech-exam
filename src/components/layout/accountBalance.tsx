a904c4e5571d33b0edc21f54751abb6fdfbec8b0
sam-j_aswatson <sam.j@webglsoft.com>
2025-05-18 17:18:00 +0800

    add error handle to balance

--- src/components/layout/accountBalance.tsx
+++ src/components/layout/accountBalance.tsx
@@ -4,6 +4,7 @@ import { Card } from "@mui/material";
 import { useState, useEffect } from "react";
 import { formatEther } from 'ethers';
 import ErrorDailog from "../ui/error";
+import { useRouter } from "next/router";
 
 interface Props {
   address:string;
@@ -11,12 +12,16 @@ interface Props {
 
 export default function AccountBalance({address}: Props) {
     const [balance , setBalance ] = useState<string>('0');
-    
+    const [errorOpen, setErrorOpen] = useState(false);
+    const router = useRouter();
     useEffect(() => {
         const fetchData = async () => {
             const list = await getBalanceByAccount(address);
-            
-            if (list.result && isNaN(Number(list.result))) {
+            if (list.status==='0') {
+                setErrorOpen(true);
+                return;
+            }
+            if (list.result && !isNaN(Number(list.result))) {
                 setBalance(list.result);
             }
             
@@ -26,6 +31,15 @@ export default function AccountBalance({address}: Props) {
     
     return (
         <div>
+            <ErrorDailog
+                isOpen={errorOpen}
+                errorContent="No found balance for this address"
+                errorTitle="Something went wrong!"
+                onClose={() => {
+                    router.push('/');
+                }}
+                closeText="Close"
+            />
             <Card style={{ margin: '10px', padding: '10px' }}>
                 <h3 className='text-2xl'>Account Address: {address}</h3>
                 <p>Balance: <b className=" text-orange-600">{balance!=='0'?formatEther(balance):balance} ETH</b></p>
